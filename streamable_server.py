c153ac2c549a8b2a279da6634a03cbe62bc419ab
getsneoguri <getsneoguri@gmail.com>
2026-04-09 14:15:52 +0900

    Update streamable_server.py

--- streamable_server.py
+++ streamable_server.py
@@ -4,6 +4,7 @@ sys.path.insert(0, '/app')
 import os
 os.environ["FASTMCP_HOST"] = "0.0.0.0"
 os.environ["FASTMCP_PORT"] = "6274"
+os.environ["FASTMCP_JSON_RESPONSE"] = "true"
 
 from mcp_kipris.server import mcp
 
